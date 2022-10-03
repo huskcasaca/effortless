1f7eac41d1deb0e9a1bf319c73fc26f6424a0927
huskcasaca <100605532+huskcasaca@users.noreply.github.com>
2022-10-03 14:00:54 +0800

    chore: Move curseforge publish to upper level

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,6 @@
-import io.github.themrmilchmann.gradle.publish.curseforge.*
-import org.gradle.jvm.tasks.Jar
-import java.util.Properties
-import java.io.*
-
 @Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.loom)
-    alias(libs.plugins.curseforge)
 }
 
 version = "1.3.0"
@@ -46,39 +40,6 @@ loom {
     }
 }
 
-publishing {
-    repositories {
-        curseForge {
-            apiKey.set(getLocalPropertyOrNull("curseforge.apikey"))
-        }
-    }
-
-    publications.create<CurseForgePublication>("curseForge") {
-
-        projectID.set(getLocalPropertyOrNull("curseforge.id")?.toInt()) // The CurseForge project ID (required)
-
-        includeGameVersions { type, version -> type == "modloader" && version == "fabric" }
-
-        val java = project.extensions.getByType(JavaPluginExtension::class)
-        val targetVersion = java.targetCompatibility.majorVersion
-
-        includeGameVersions { type, version -> type == "java" && version == "java-$targetVersion" }
-
-        val mcVersion = libs.versions.minecraft.get()
-        val matchGroups = mcVersion.split(".")
-        val mcDependencySlug = "minecraft-${matchGroups.take(2).joinToString("-")}"
-        val mcVersionSlug = matchGroups.take(3).joinToString("-")
-
-        includeGameVersions { type, version -> type == mcDependencySlug && version == mcVersionSlug }
-
-        artifact {
-            changelog = Changelog("Changelog...", ChangelogType.TEXT) // The changelog (required)
-            releaseType = ReleaseType.RELEASE // The release type (required)
-            displayName = "${base.archivesName.get()}-${version}-${libs.versions.minecraft.get()}.jar"
-        }
-    }
-}
-
 tasks {
     withType(JavaCompile::class) {
         options.release.set(JavaVersion.VERSION_17.toString().toInt())
@@ -93,17 +54,4 @@ tasks {
     withType(Jar::class) {
         archiveVersion.set("${project.version}-${libs.versions.minecraft.get()}")
     }
-}
-
-fun getLocalPropertyOrNull(key: String): String? {
-    val properties = Properties()
-    val localProperties = File("local.properties")
-    if (localProperties.isFile) {
-        InputStreamReader(FileInputStream(localProperties), Charsets.UTF_8).use { reader ->
-            properties.load(reader)
-        }
-    } else {
-        println(localProperties.name + " is not found")
-    }
-    return properties.getProperty(key)
 }
