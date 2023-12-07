e3aecaa8a607ca9a229f12bcc0f81abaee8d1b60
huskcasaca <100605532+huskcasaca@users.noreply.github.com>
2023-12-07 11:04:59 +0800

    fix(core): UI adjustments

--- client/src/main/java/dev/huskuraft/effortless/screen/transformer/EffortlessTransformerEditScreen.java
+++ client/src/main/java/dev/huskuraft/effortless/screen/transformer/EffortlessTransformerEditScreen.java
@@ -3,7 +3,12 @@ package dev.huskuraft.effortless.screen.transformer;
 import dev.huskuraft.effortless.building.pattern.Transformer;
 import dev.huskuraft.effortless.core.Entrance;
 import dev.huskuraft.effortless.gui.AbstractScreen;
+import dev.huskuraft.effortless.gui.AbstractWidget;
+import dev.huskuraft.effortless.gui.Dimens;
 import dev.huskuraft.effortless.gui.button.Button;
+import dev.huskuraft.effortless.gui.input.EditBox;
+import dev.huskuraft.effortless.gui.slot.TextSlot;
+import dev.huskuraft.effortless.gui.text.TextWidget;
 import dev.huskuraft.effortless.text.Text;
 
 import java.util.function.Consumer;
@@ -12,9 +17,12 @@ public class EffortlessTransformerEditScreen extends AbstractScreen {
 
     private final Consumer<Transformer> applySettings;
     private final Transformer defaultSettings;
+    private AbstractWidget titleTextWidget;
     private TransformerList entries;
     private Button saveButton;
     private Button cancelButton;
+    private TextSlot textSlot;
+    private EditBox nameEditBox;
 
     public EffortlessTransformerEditScreen(Entrance entrance, Consumer<Transformer> consumer, Transformer transformer) {
         super(entrance, Text.translate("effortless.transformer.edit.title"));
@@ -24,8 +32,19 @@ public class EffortlessTransformerEditScreen extends AbstractScreen {
 
     @Override
     public void onCreate() {
-        this.entries = addWidget(new TransformerList(getEntrance(), 0, 50, getWidth(), getHeight() - 50 - 36, this, false, TransformerList.EntryType.EDITOR));
+        this.titleTextWidget = addWidget(new TextWidget(getEntrance(), getWidth() / 2, 24 - 16, getScreenTitle(), TextWidget.Gravity.CENTER));
+
+        this.textSlot = addWidget(new TextSlot(getEntrance(), getWidth() / 2 - (Dimens.RegularEntry.ROW_WIDTH) / 2, 16 + 2, Dimens.ICON_WIDTH, Dimens.ICON_HEIGHT, Text.empty()));
+
+        this.nameEditBox = addWidget(
+                new EditBox(getEntrance(), getWidth() / 2 - (Dimens.RegularEntry.ROW_WIDTH) / 2 + 40, 24, Dimens.RegularEntry.ROW_WIDTH - 40, 20, null)
+        );
+        this.nameEditBox.setMaxLength(255);
+        this.nameEditBox.setHint(Text.translate("effortless.randomizer.edit.name_hint"));
+
+        this.entries = addWidget(new TransformerList(getEntrance(), 0, 54, getWidth(), getHeight() - 54 - 36, this, false, TransformerList.EntryType.EDITOR));
         this.entries.insertSelected(defaultSettings);
+        this.entries.setRenderSelection(false);
 
         this.saveButton = addWidget(Button.builder(getEntrance(), Text.translate("effortless.transformer.edit.save"), button -> {
             applySettings.accept(entries.getSelected().getItem());
@@ -36,4 +55,8 @@ public class EffortlessTransformerEditScreen extends AbstractScreen {
         }).bounds(getWidth() / 2 + 4, getHeight() - 28, 150, 20).build());
     }
 
+    @Override
+    public void onReload() {
+        super.onReload();
+    }
 }
