ff5f20ba475b41523407e997c0e763d57dfd2d0f
huskcasaca <100605532+huskcasaca@users.noreply.github.com>
2024-01-03 15:01:30 +0800

    feat(core): Math Vector4i Vector4d Matrix3f Matrix4f

---
+++ common/src/main/java/dev/huskuraft/effortless/math/Matrix3f.java
@@ -0,0 +1,9 @@
+package dev.huskuraft.effortless.math;
+
+public record Matrix3f(
+        float m00, float m01, float m02,
+        float m10, float m11, float m12,
+        float m20, float m21, float m22
+) {
+
+}
---
+++ common/src/main/java/dev/huskuraft/effortless/math/Matrix4f.java
@@ -0,0 +1,64 @@
+package dev.huskuraft.effortless.math;
+
+
+public record Matrix4f(
+        float m00, float m01, float m02, float m03,
+        float m10, float m11, float m12, float m13,
+        float m20, float m21, float m22, float m23,
+        float m30, float m31, float m32, float m33
+) {
+
+
+    /**
+     * Bit returned by {@link #properties()} to indicate that the matrix represents a perspective transformation.
+     */
+    public static byte PROPERTY_PERSPECTIVE = 1<<0;
+    /**
+     * Bit returned by {@link #properties()} to indicate that the matrix represents an affine transformation.
+     */
+    public static byte PROPERTY_AFFINE = 1<<1;
+    /**
+     * Bit returned by {@link #properties()} to indicate that the matrix represents the identity transformation.
+     */
+    public static byte PROPERTY_IDENTITY = 1<<2;
+    /**
+     * Bit returned by {@link #properties()} to indicate that the matrix represents a pure translation transformation.
+     */
+    public static byte PROPERTY_TRANSLATION = 1<<3;
+    /**
+     * Bit returned by {@link #properties()} to indicate that the upper-left 3x3 submatrix represents an orthogonal
+     * matrix (i.e. orthonormal basis). For practical reasons, this property also always implies
+     * {@link #PROPERTY_AFFINE} in this implementation.
+     */
+    public static byte PROPERTY_ORTHONORMAL = 1<<4;
+
+    public int properties() {
+        int properties = 0;
+        if (m03() == 0.0f && m13() == 0.0f) {
+            if (m23() == 0.0f && m33() == 1.0f) {
+                properties |= PROPERTY_AFFINE;
+                if (m00() == 1.0f && m01() == 0.0f && m02() == 0.0f && m10() == 0.0f && m11() == 1.0f && m12() == 0.0f
+                        && m20() == 0.0f && m21() == 0.0f && m22() == 1.0f) {
+                    properties |= PROPERTY_TRANSLATION | PROPERTY_ORTHONORMAL;
+                    if (m30() == 0.0f && m31() == 0.0f && m32() == 0.0f)
+                        properties |= PROPERTY_IDENTITY;
+                }
+                /*
+                 * We do not determine orthogonality, since it would require arbitrary epsilons
+                 * and is rather expensive (6 dot products) in the worst case.
+                 */
+            } else if (m01() == 0.0f && m02() == 0.0f && m10() == 0.0f && m12() == 0.0f && m20() == 0.0f && m21() == 0.0f
+                    && m30() == 0.0f && m31() == 0.0f && m33() == 0.0f) {
+                properties |= PROPERTY_PERSPECTIVE;
+            }
+        }
+        return properties;
+    }
+
+    public Vector4f mul(Vector4f vector4f) {
+        if ((properties() & PROPERTY_AFFINE) != 0)
+            return mulAffine(this, vector4f);
+        return mulGeneric(this, vector4f);
+    }
+
+}
