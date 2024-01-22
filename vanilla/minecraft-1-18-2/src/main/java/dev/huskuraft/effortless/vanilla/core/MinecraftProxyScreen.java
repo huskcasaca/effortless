03abf18435a36c188881487086268db126c622b8
huskcasaca <100605532+huskcasaca@users.noreply.github.com>
2024-01-22 14:24:38 +0800

    feat(port): Port to 1.18.2

---
+++ vanilla/minecraft-1-18-2/src/main/java/dev/huskuraft/effortless/vanilla/core/MinecraftProxyScreen.java
@@ -0,0 +1,131 @@
+package dev.huskuraft.effortless.vanilla.core;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import dev.huskuraft.effortless.api.gui.Screen;
+import dev.huskuraft.effortless.vanilla.renderer.MinecraftRenderer;
+import net.minecraft.network.chat.Component;
+
+public class MinecraftProxyScreen extends net.minecraft.client.gui.screens.Screen {
+
+    private final Screen proxy;
+
+    public MinecraftProxyScreen(Screen screen) {
+        super(Component.empty());
+        this.proxy = screen;
+    }
+
+    public Screen getProxy() {
+        return proxy;
+    }
+
+    @Override
+    public void tick() {
+        proxy.tick();
+    }
+
+    @Override
+    protected void init() {
+        proxy.setWidth(width);
+        proxy.setHeight(height);
+        proxy.onDestroy();
+        proxy.onCreate();
+        proxy.onLoad();
+    }
+
+    @Override
+    protected void rebuildWidgets() {
+        proxy.setWidth(width);
+        proxy.setHeight(height);
+        proxy.onDestroy();
+        proxy.onCreate();
+        proxy.onLoad();
+    }
+
+    @Override
+    public void removed() {
+        proxy.onDestroy();
+    }
+
+    @Override
+    public void onClose() {
+    }
+
+    @Override
+    public void render(PoseStack minecraftMatrixStack, int i, int j, float f) {
+        var renderer = new MinecraftRenderer(minecraftMatrixStack);
+        proxy.onReload();
+        proxy.render(renderer, i, j, f);
+        proxy.renderOverlay(renderer, i, j, f);
+    }
+
+    @Override
+    public boolean isPauseScreen() {
+        return proxy.isPauseGame();
+    }
+
+    @Override
+    public void mouseMoved(double d, double e) {
+        proxy.onMouseMoved(d, e);
+    }
+
+    @Override
+    public boolean mouseClicked(double d, double e, int i) {
+        return proxy.onMouseClicked(d, e, i);
+    }
+
+    @Override
+    public boolean mouseReleased(double d, double e, int i) {
+        return proxy.onMouseReleased(d, e, i);
+    }
+
+    @Override
+    public boolean mouseDragged(double d, double e, int i, double f, double g) {
+        return proxy.onMouseDragged(d, e, i, f, g);
+    }
+
+    @Override
+    public boolean mouseScrolled(double d, double e, double f) {
+        return proxy.onMouseScrolled(d, e, f, 0);
+    }
+
+    @Override
+    public boolean keyPressed(int i, int j, int k) {
+        return proxy.onKeyPressed(i, j, k);
+    }
+
+    @Override
+    public boolean keyReleased(int i, int j, int k) {
+        return proxy.onKeyReleased(i, j, k);
+    }
+
+    @Override
+    public boolean charTyped(char c, int i) {
+        return proxy.onCharTyped(c, i);
+    }
+
+    //    @Override
+    @Deprecated
+    public boolean changeFocus(boolean bl) {
+        return proxy.onFocusMove(bl);
+    }
+
+    @Override
+    public boolean isMouseOver(double d, double e) {
+        return proxy.isMouseOver(d, e);
+    }
+
+    @Override
+    public Component getTitle() {
+        return proxy.getScreenTitle().reference();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof MinecraftProxyScreen keyBinding && proxy.equals(keyBinding.proxy);
+    }
+
+    @Override
+    public int hashCode() {
+        return proxy.hashCode();
+    }
+}
---
+++ vanilla/minecraft-1-18-2/src/main/java/dev/huskuraft/effortless/vanilla/platform/MinecraftCommonContentFactory.java
@@ -0,0 +1,114 @@
+package dev.huskuraft.effortless.vanilla.platform;
+
+import dev.huskuraft.effortless.api.core.Item;
+import dev.huskuraft.effortless.api.core.ItemStack;
+import dev.huskuraft.effortless.api.core.ResourceLocation;
+import dev.huskuraft.effortless.api.networking.Buffer;
+import dev.huskuraft.effortless.api.platform.ContentFactory;
+import dev.huskuraft.effortless.api.platform.Platform;
+import dev.huskuraft.effortless.api.tag.TagIoReader;
+import dev.huskuraft.effortless.api.tag.TagIoWriter;
+import dev.huskuraft.effortless.api.tag.TagRecord;
+import dev.huskuraft.effortless.api.text.Text;
+import dev.huskuraft.effortless.vanilla.core.*;
+import dev.huskuraft.effortless.vanilla.tag.MinecraftTagRecord;
+import io.netty.buffer.Unpooled;
+import net.minecraft.Util;
+import net.minecraft.core.DefaultedRegistry;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtIo;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.chat.Component;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public class MinecraftCommonContentFactory implements ContentFactory {
+
+    public static final MinecraftCommonContentFactory INSTANCE = new MinecraftCommonContentFactory();
+
+    @Override
+    public ResourceLocation newResource(String namespace, String path) {
+        return new MinecraftResourceLocation(new net.minecraft.resources.ResourceLocation(namespace, path));
+    }
+
+    @Override
+    public Buffer newBuffer() {
+        return new MinecraftBuffer(new FriendlyByteBuf(Unpooled.buffer()));
+    }
+
+    @Override
+    public TagRecord newTagRecord() {
+        return new MinecraftTagRecord(new CompoundTag());
+    }
+
+    @Override
+    public Optional<Item> newOptionalItem(ResourceLocation location) {
+        return DefaultedRegistry.ITEM.getOptional(location.<net.minecraft.resources.ResourceLocation>reference()).map(item -> new MinecraftItem(item));
+    }
+
+    @Override
+    public ItemStack newItemStack() {
+        return new MinecraftItemStack(net.minecraft.world.item.ItemStack.EMPTY);
+    }
+
+    @Override
+    public ItemStack newItemStack(Item item, int count) {
+        net.minecraft.world.item.Item item1 = item.reference();
+        return new MinecraftItemStack(item1, count);
+    }
+
+    @Override
+    public ItemStack newItemStack(Item item, int count, TagRecord tag) {
+        net.minecraft.world.item.Item item1 = item.reference();
+        CompoundTag tag1 = tag.reference();
+        return new MinecraftItemStack(item1, tag1, count);
+    }
+
+    @Override
+    public Text newText() {
+        return new MinecraftText((Component) Component.empty());
+    }
+
+    @Override
+    public Text newText(String text) {
+        return new MinecraftText((Component) Component.literal(text));
+    }
+
+    @Override
+    public Text newText(String text, Text... args) {
+        return new MinecraftText((Component) Component.translatable(text, Arrays.stream(args).map(text1 -> text1.reference()).toArray(Object[]::new)));
+    }
+
+    @Override
+    public Text newTranslatableText(String text) {
+        return new MinecraftText((Component) Component.translatable(text));
+    }
+
+    @Override
+    public Text newTranslatableText(String text, Text... args) {
+        return new MinecraftText((Component) Component.translatable(text, Arrays.stream(args).map(text1 -> text1.reference()).toArray(Object[]::new)));
+    }
+
+    @Override
+    public TagIoReader getTagIoReader() {
+        return input -> new MinecraftTagRecord(NbtIo.readCompressed(input));
+    }
+
+    @Override
+    public TagIoWriter getTagIoWriter() {
+        return (output, config) -> NbtIo.writeCompressed(config.reference(), output);
+    }
+
+    @Override
+    public Platform.OperatingSystem getOperatingSystem() {
+        return switch (Util.getPlatform()) {
+            case LINUX -> Platform.OperatingSystem.LINUX;
+            case SOLARIS -> Platform.OperatingSystem.SOLARIS;
+            case WINDOWS -> Platform.OperatingSystem.WINDOWS;
+            case OSX -> Platform.OperatingSystem.MACOS;
+            case UNKNOWN -> Platform.OperatingSystem.UNKNOWN;
+        };
+    }
+
+}
