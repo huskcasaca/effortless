1e977d8ea1d426d0ef34f79d6b4fc074eb02f7ad
huskcasaca <100605532+huskcasaca@users.noreply.github.com>
2024-01-25 13:05:05 +0800

    build: Use fabric nestedJars

---
+++ fabric/src/main/java/dev/huskuraft/effortless/fabric/FabricEffortlessUniversal.java
@@ -0,0 +1,11 @@
+package dev.huskuraft.effortless.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricEffortlessUniversal implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+
+}
