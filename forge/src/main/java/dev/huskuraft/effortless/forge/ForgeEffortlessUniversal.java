7afc2f9420b1a5c286403d2dc9e4c3ddd5a92964
huskcasaca <100605532+huskcasaca@users.noreply.github.com>
2024-01-25 19:39:12 +0800

    build: Prepare for forge universal

---
+++ forge/src/main/java/dev/huskuraft/effortless/forge/ForgeEffortlessUniversal.java
@@ -0,0 +1,15 @@
+package dev.huskuraft.effortless.forge;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.loading.FMLLoader;
+
+public class ForgeEffortlessUniversal {
+
+	static {
+		System.out.println("forge_effortless");
+		System.out.println("forge_effortless mc_version " + FMLLoader.versionInfo().mcVersion());
+
+
+	}
+
+}
