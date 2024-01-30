3d525abe82d10a6f8919703f8651f71dff084517
huskcasaca <100605532+huskcasaca@users.noreply.github.com>
2024-01-30 23:36:31 +0800

    refactor: Rename modules

---
+++ fabric/mc-1-20-2/src/main/java/dev/huskuraft/effortless/fabric/mixin/GuiMixin.java
@@ -0,0 +1,20 @@
+package dev.huskuraft.effortless.fabric.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import dev.huskuraft.effortless.fabric.events.ClientRenderEvents;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiGraphics;
+
+@Mixin(Gui.class)
+public abstract class GuiMixin {
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Gui;renderSavingIndicator(Lnet/minecraft/client/gui/GuiGraphics;)V", shift = At.Shift.AFTER))
+    private void onRenderGui(GuiGraphics guiGraphics, float f, CallbackInfo ci) {
+        ClientRenderEvents.GUI.invoker().onRenderGui(guiGraphics, f);
+    }
+
+}
