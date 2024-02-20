2527f31b1531e9bcad5921bd86ce7437646e355b
huskcasaca <100605532+huskcasaca@users.noreply.github.com>
2024-02-20 22:41:41 +0800

    fix: PlatformLoader

--- api/src/main/java/dev/huskuraft/effortless/api/platform/PlatformLoader.java
+++ api/src/main/java/dev/huskuraft/effortless/api/platform/PlatformLoader.java
@@ -4,7 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
@@ -14,13 +16,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.ServiceConfigurationError;
 import java.util.Spliterators;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-public final class PlatformLoader<S> implements Iterable<Optional<S>> {
+public final class PlatformLoader<S> implements Iterable<PlatformLoader.Loader<S>> {
 
     private static final String PREFIX = "META-INF/services/";
     private static final Map<Class<?>, Object> INSTANCES = Collections.synchronizedMap((new HashMap<>()));
@@ -30,64 +31,6 @@ public final class PlatformLoader<S> implements Iterable<Optional<S>> {
     private PlatformLoader(Class<S> svc, ClassLoader cl) {
         service = Objects.requireNonNull(svc, "Service interface cannot be null");
         loader = (cl == null) ? ClassLoader.getSystemClassLoader() : cl;
-    }
-
-    private static void fail(Class<?> service, String msg, Throwable cause) throws ServiceConfigurationError {
-        throw new ServiceConfigurationError(service.getName() + ": " + msg, cause);
-    }
-
-    private static void fail(Class<?> service, String msg) throws ServiceConfigurationError {
-        throw new ServiceConfigurationError(service.getName() + ": " + msg);
-    }
-
-    private static void fail(Class<?> service, URL u, int line, String msg) throws ServiceConfigurationError {
-        fail(service, u + ":" + line + ": " + msg);
-    }
-
-    private static int parseLine(Class<?> service, URL u, BufferedReader r, int lc, List<String> names) throws IOException, ServiceConfigurationError {
-        String ln = r.readLine();
-        if (ln == null) {
-            return -1;
-        }
-        int ci = ln.indexOf('#');
-        if (ci >= 0) ln = ln.substring(0, ci);
-        ln = ln.trim();
-        int n = ln.length();
-        if (n != 0) {
-            if ((ln.indexOf(' ') >= 0) || (ln.indexOf('\t') >= 0))
-                fail(service, u, lc, "Illegal configuration-file syntax");
-            int cp = ln.codePointAt(0);
-            if (!Character.isJavaIdentifierStart(cp)) fail(service, u, lc, "Illegal provider-class name: " + ln);
-            for (int i = Character.charCount(cp); i < n; i += Character.charCount(cp)) {
-                cp = ln.codePointAt(i);
-                if (!Character.isJavaIdentifierPart(cp) && (cp != '.'))
-                    fail(service, u, lc, "Illegal provider-class name: " + ln);
-            }
-            if (!names.contains(ln)) names.add(ln);
-        }
-        return lc + 1;
-    }
-
-    private static Iterator<String> parse(Class<?> service, URL url) throws ServiceConfigurationError {
-        InputStream inputStream = null;
-        BufferedReader reader = null;
-        var names = new ArrayList<String>();
-        try {
-            inputStream = url.openStream();
-            reader = new BufferedReader(new InputStreamReader(inputStream, "utf-8"));
-            int lc = 1;
-            while ((lc = parseLine(service, url, reader, lc, names)) >= 0) ;
-        } catch (IOException x) {
-            fail(service, "Error reading configuration file", x);
-        } finally {
-            try {
-                if (reader != null) reader.close();
-                if (inputStream != null) inputStream.close();
-            } catch (IOException y) {
-                fail(service, "Error closing configuration file", y);
-            }
-        }
-        return names.iterator();
     }
 
     public static <S> PlatformLoader<S> load(Class<S> service, ClassLoader loader) {
@@ -109,18 +52,6 @@ public final class PlatformLoader<S> implements Iterable<Optional<S>> {
         return PlatformLoader.load(service, prev);
     }
 
-    private static LoaderType getLoaderTypeByThread() {
-        var loader = Thread.currentThread().getContextClassLoader();
-        if (loader.getClass().getPackageName().equals("net.fabricmc.loader.impl.launch.knot")) {
-            return LoaderType.FABRIC;
-        }
-        var p = loader.getClass().getPackageName();
-        if (loader.getClass().getPackageName().equals("cpw.mods.modlauncher")) {
-            return LoaderType.FORGE;
-        }
-        throw new IllegalStateException("Unknown loader: " + p);
-    }
-
     public static <S> S getSingleton(Class<S> service) {
         if (!INSTANCES.containsKey(service)) {
             INSTANCES.put(service, load(service).get());
@@ -132,133 +63,185 @@ public final class PlatformLoader<S> implements Iterable<Optional<S>> {
         return getSingleton((Class<S>) typeGetter.getClass().getComponentType());
     }
 
-    public Iterator<Optional<S>> iterator() {
-        return new Iterator<>() {
-
-            final LazyIterator lookupIterator = new LazyIterator(service, loader);
-
-            public boolean hasNext() {
-                return lookupIterator.hasNext();
-            }
-
-            public Optional<S> next() {
-                try {
-                    return Optional.ofNullable(lookupIterator.next());
-                } catch (ServiceConfigurationError e) {
-                    return Optional.empty();
-                }
-            }
-
-            public void remove() {
-                throw new UnsupportedOperationException();
-            }
-
-        };
+    public Iterator<Loader<S>> iterator() {
+        return new LazyIterator<S>(service, loader);
     }
 
-    public Stream<Optional<S>> stream() {
+    public Stream<Loader<S>> stream() {
         return StreamSupport.stream(Spliterators.spliteratorUnknownSize(iterator(), 0), false);
     }
 
-    public Optional<S> findFirst() {
-        return stream().filter(Optional::isPresent).findFirst().orElseThrow();
+    public Loader<S> findFirst() {
+        return stream().filter(Loader::isPresent).findFirst().orElseThrow();
     }
 
     public S get() {
-        return findFirst().orElseThrow();
-    }
-
-    public S getOrNull() {
-        return findFirst().orElse(null);
+        return findFirst().get();
     }
 
     public String toString() {
         return "PlatformLoader[" + service.getName() + "]";
     }
 
-    private class LazyIterator implements Iterator<S> {
+    public record Loader<S>(Class<S> clazz, String className, ClassLoader loader) {
 
-        Class<S> service;
-        ClassLoader loader;
-        Enumeration<URL> configs = null;
-        Iterator<String> pending = null;
-        String nextName = null;
+        public LoaderType getClassTypeByName() {
+            if (className.contains("vanilla")) {
+                return LoaderType.VANILLA;
+            }
+            if (className.contains("fabric")) {
+                return LoaderType.FABRIC;
+            }
+            if (className.contains("forge")) {
+                return LoaderType.FORGE;
+            }
+            return LoaderType.VANILLA;
+        }
 
-        private LazyIterator(Class<S> service, ClassLoader loader) {
-            this.service = service;
+        private S create() throws ClassNotFoundException, ClassCastException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+            var childClass = Class.forName(className, false, loader);
+            if (!clazz.isAssignableFrom(childClass)) {
+                throw new ClassCastException(className + " is not a subtype of " + clazz.getName());
+            }
+            return clazz.cast(childClass.getDeclaredConstructor().newInstance());
+        }
+
+        public boolean isPresent() {
+            if (getClassTypeByName() == LoaderType.VANILLA) {
+                return true;
+            } else {
+                return getClassTypeByName() == getLoaderTypeByThread();
+            }
+        }
+
+        public S get() {
+            if (!isPresent()) {
+                throw new ServiceConfigurationError("Service " + clazz.getName() + " is not available in loader " + getLoaderTypeByThread());
+            }
+            try {
+                return create();
+            } catch (Exception e) {
+                throw new ServiceConfigurationError(e.getMessage());
+            }
+        }
+
+        private static LoaderType getLoaderTypeByThread() {
+            var loader = Thread.currentThread().getContextClassLoader();
+            if (loader.getClass().getPackageName().equals("net.fabricmc.loader.impl.launch.knot")) {
+                return LoaderType.FABRIC;
+            }
+            var p = loader.getClass().getPackageName();
+            if (loader.getClass().getPackageName().equals("cpw.mods.modlauncher")) {
+                return LoaderType.FORGE;
+            }
+            throw new IllegalStateException("Unknown loader: " + p);
+        }
+
+    }
+
+    private static class LazyIterator<S> implements Iterator<Loader<S>> {
+
+        private final Class<S> clazz;
+        private final ClassLoader loader;
+        private Enumeration<URL> configs = null;
+        private Iterator<String> pending = null;
+        private String nextClassName = null;
+
+        private LazyIterator(Class<S> clazz, ClassLoader loader) {
+            this.clazz = clazz;
             this.loader = loader;
         }
 
-        private boolean hasNextService() {
-            if (nextName != null) {
+        public boolean hasNext() {
+            if (nextClassName != null) {
                 return true;
             }
             if (configs == null) {
                 try {
-                    String fullName = PREFIX + service.getName();
+                    String fullName = PREFIX + clazz.getName();
                     if (loader == null) configs = ClassLoader.getSystemResources(fullName);
                     else configs = loader.getResources(fullName);
                 } catch (IOException x) {
-                    fail(service, "Error locating configuration files", x);
+                    fail(clazz, "Error locating configuration files", x);
                 }
             }
-            while ((pending == null) || !pending.hasNext()) {
+            while (pending == null || !pending.hasNext()) {
                 if (!configs.hasMoreElements()) {
                     return false;
                 }
-                pending = parse(service, configs.nextElement());
+                pending = parse(clazz, configs.nextElement());
             }
-            nextName = pending.next();
+            nextClassName = pending.next();
             return true;
         }
 
-        private S nextService() {
-            if (!hasNextService()) throw new NoSuchElementException();
-            String cn = nextName;
-            nextName = null;
-            Class<?> c = null;
-            try {
-                c = Class.forName(cn, false, loader);
-            } catch (ClassNotFoundException x) {
-                fail(service, "Provider " + cn + " not found");
-            }
-            if (!service.isAssignableFrom(c)) {
-                fail(service, "Provider " + cn + " not a subtype");
-            }
-            try {
-                var casted = service.cast(c.getDeclaredConstructor().newInstance());
-
-                if (casted.getClass().getPackageName().contains("vanilla")) {
-                    return casted;
-                }
-                if (casted.getClass().getPackageName().contains("fabric")) {
-                    if (getLoaderTypeByThread() == LoaderType.FABRIC) {
-                        return casted;
-                    }
-                }
-                if (casted.getClass().getPackageName().contains("forge")) {
-                    if (getLoaderTypeByThread() == LoaderType.FORGE) {
-                        return casted;
-                    }
-                }
-                return casted;
-            } catch (Throwable x) {
-                x.printStackTrace();
-                fail(service, "Provider " + cn + " could not be instantiated", x);
-            }
-            throw new Error();          // This cannot happen
-        }
-
-        public boolean hasNext() {
-            return hasNextService();
-        }
-
-        public S next() {
-            return nextService();
+        public Loader<S> next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            var className = nextClassName;
+            nextClassName = null;
+            return new Loader<>(clazz, className, loader);
         }
 
         public void remove() {
             throw new UnsupportedOperationException();
+        }
+
+        private static int parseLine(Class<?> service, URL u, BufferedReader r, int lc, List<String> names) throws IOException, ServiceConfigurationError {
+            var ln = r.readLine();
+            if (ln == null) {
+                return -1;
+            }
+            int ci = ln.indexOf('#');
+            if (ci >= 0) ln = ln.substring(0, ci);
+            ln = ln.trim();
+            int n = ln.length();
+            if (n != 0) {
+                if ((ln.indexOf(' ') >= 0) || (ln.indexOf('\t') >= 0))
+                    fail(service, u, lc, "Illegal configuration-file syntax");
+                int cp = ln.codePointAt(0);
+                if (!Character.isJavaIdentifierStart(cp)) fail(service, u, lc, "Illegal provider-class name: " + ln);
+                for (int i = Character.charCount(cp); i < n; i += Character.charCount(cp)) {
+                    cp = ln.codePointAt(i);
+                    if (!Character.isJavaIdentifierPart(cp) && (cp != '.'))
+                        fail(service, u, lc, "Illegal provider-class name: " + ln);
+                }
+                if (!names.contains(ln)) names.add(ln);
+            }
+            return lc + 1;
+        }
+
+        private static Iterator<String> parse(Class<?> service, URL url) throws ServiceConfigurationError {
+            InputStream inputStream = null;
+            BufferedReader reader = null;
+            var names = new ArrayList<String>();
+            try {
+                inputStream = url.openStream();
+                reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+                int lc = 1;
+                while ((lc = parseLine(service, url, reader, lc, names)) >= 0) ;
+            } catch (IOException x) {
+                fail(service, "Error reading configuration file", x);
+            } finally {
+                try {
+                    if (reader != null) reader.close();
+                    if (inputStream != null) inputStream.close();
+                } catch (IOException y) {
+                    fail(service, "Error closing configuration file", y);
+                }
+            }
+            return names.iterator();
+        }
+
+        private static void fail(Class<?> service, String msg, Throwable cause) throws ServiceConfigurationError {
+            throw new ServiceConfigurationError(service.getName() + ": " + msg, cause);
+        }
+
+        private static void fail(Class<?> service, String msg) throws ServiceConfigurationError {
+            throw new ServiceConfigurationError(service.getName() + ": " + msg);
+        }
+
+        private static void fail(Class<?> service, URL u, int line, String msg) throws ServiceConfigurationError {
+            fail(service, u + ":" + line + ": " + msg);
         }
 
     }
