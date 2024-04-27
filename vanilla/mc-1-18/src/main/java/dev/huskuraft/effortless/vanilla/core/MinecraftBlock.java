57c9882254396df2f536f6c5858211667ba74452
huskcasaca <100605532+huskcasaca@users.noreply.github.com>
2024-04-27 16:16:35 +0800

    feat: BucketCollectable & LiquidPlaceable

--- vanilla/mc-1-18/src/main/java/dev/huskuraft/effortless/vanilla/core/MinecraftBlock.java
+++ vanilla/mc-1-18/src/main/java/dev/huskuraft/effortless/vanilla/core/MinecraftBlock.java
@@ -1,33 +1,64 @@
 package dev.huskuraft.effortless.vanilla.core;
 
+import java.util.Optional;
+
 import dev.huskuraft.effortless.api.core.Block;
+import dev.huskuraft.effortless.api.core.BlockPosition;
 import dev.huskuraft.effortless.api.core.BlockState;
+import dev.huskuraft.effortless.api.core.FluidState;
+import dev.huskuraft.effortless.api.core.ItemStack;
+import dev.huskuraft.effortless.api.core.Player;
+import dev.huskuraft.effortless.api.core.World;
+import dev.huskuraft.effortless.api.core.fluid.BucketCollectable;
+import dev.huskuraft.effortless.api.core.fluid.Fluid;
+import dev.huskuraft.effortless.api.core.fluid.LiquidPlaceable;
+import dev.huskuraft.effortless.api.sound.Sound;
+import dev.huskuraft.effortless.vanilla.sound.MinecraftSound;
+import net.minecraft.world.level.block.LiquidBlockContainer;
 
-public class MinecraftBlock implements Block {
-
-    private final net.minecraft.world.level.block.Block reference;
-
-    public MinecraftBlock(net.minecraft.world.level.block.Block reference) {
-        this.reference = reference;
-    }
-
-    @Override
-    public net.minecraft.world.level.block.Block referenceValue() {
-        return reference;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return obj instanceof MinecraftBlock obj1 && reference.equals(obj1.reference);
-    }
-
-    @Override
-    public int hashCode() {
-        return reference.hashCode();
-    }
+public record MinecraftBlock(
+        net.minecraft.world.level.block.Block referenceValue
+) implements Block {
 
     @Override
     public BlockState getDefaultBlockState() {
         return MinecraftBlockState.ofNullable(referenceValue().defaultBlockState());
+    }
+
+    @Override
+    public BucketCollectable getBucketCollectable() {
+        if (referenceValue() instanceof net.minecraft.world.level.block.BucketPickup bucketPickup) {
+            return new BucketCollectable() {
+
+                @Override
+                public ItemStack pickupBlock(World world, Player player, BlockPosition blockPosition, BlockState blockState) {
+                    return new MinecraftItemStack(bucketPickup.pickupBlock(world.reference(), MinecraftConvertor.toPlatformBlockPosition(blockPosition), blockState.reference()));
+                }
+
+                @Override
+                public Optional<Sound> getPickupSound() {
+                    return bucketPickup.getPickupSound().map(MinecraftSound::new);
+                }
+            };
+        }
+        return null;
+    }
+
+    @Override
+    public LiquidPlaceable getLiquidPlaceable() {
+        if (referenceValue() instanceof LiquidBlockContainer liquidPlace) {
+            return new LiquidPlaceable() {
+                @Override
+                public boolean canPlaceLiquid(World world, Player player, BlockPosition blockPosition, BlockState blockState, Fluid fluid) {
+                    return liquidPlace.canPlaceLiquid(world.reference(), MinecraftConvertor.toPlatformBlockPosition(blockPosition), blockState.reference(), fluid.reference());
+                }
+
+                @Override
+                public boolean placeLiquid(World world, BlockPosition blockPosition, BlockState blockState, FluidState fluidState) {
+                    return liquidPlace.placeLiquid(world.reference(), MinecraftConvertor.toPlatformBlockPosition(blockPosition), blockState.reference(), fluidState.reference());
+                }
+            };
+        }
+        return null;
     }
 }
--- vanilla/mc-1-19-2/src/main/java/dev/huskuraft/effortless/vanilla/core/MinecraftBlockItem.java
+++ vanilla/mc-1-19-2/src/main/java/dev/huskuraft/effortless/vanilla/core/MinecraftBlockItem.java
@@ -2,21 +2,23 @@ package dev.huskuraft.effortless.vanilla.core;
 
 import dev.huskuraft.effortless.api.core.Block;
 import dev.huskuraft.effortless.api.core.BlockItem;
+import dev.huskuraft.effortless.api.core.ItemStack;
+import dev.huskuraft.effortless.api.core.ResourceLocation;
 
-public class MinecraftBlockItem extends MinecraftItem implements BlockItem {
-
-    public MinecraftBlockItem(net.minecraft.world.item.BlockItem reference) {
-        super(reference);
-    }
+public record MinecraftBlockItem(net.minecraft.world.item.BlockItem referenceValue) implements BlockItem {
 
     @Override
-    public net.minecraft.world.item.BlockItem referenceValue() {
-        return (net.minecraft.world.item.BlockItem) super.referenceValue();
+    public ItemStack getDefaultStack() {
+        return new MinecraftItem(referenceValue()).getDefaultStack();
     }
 
     @Override
     public Block getBlock() {
-        return new MinecraftBlock(referenceValue().getBlock());
+        return new MinecraftItem(referenceValue()).getBlock();
     }
 
+    @Override
+    public ResourceLocation getId() {
+        return new MinecraftItem(referenceValue()).getId();
+    }
 }
