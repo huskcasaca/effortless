bcb3fef05204cae391c5bcafd390d75a5dc91788
huskcasaca <100605532+huskcasaca@users.noreply.github.com>
2024-04-30 21:56:01 +0800

    fix: Use item not award statistics

--- api/src/main/java/dev/huskuraft/effortless/api/core/BucketItem.java
+++ api/src/main/java/dev/huskuraft/effortless/api/core/BucketItem.java
@@ -26,7 +26,6 @@ public interface BucketItem extends Item {
                 if (bucketCollectable != null) {
                     var collected = bucketCollectable.pickupBlock(player.getWorld(), player, blockInteraction.getBlockPosition(), blockState);
                     if (!collected.isEmpty()) {
-                        player.awardStat(StatTypes.ITEM_USED.get(bucketItem));
                         var result = createFilledResult(player, itemStack, collected);
                         return InteractionResult.SUCCESS;
                     }
@@ -35,7 +34,6 @@ public interface BucketItem extends Item {
                 if (blockState.getBlock().getLiquidPlaceable() != null || blockState.isAir() || blockState.canReplace(bucketItem.getContent())) {
                     if (bucketItem.useContent(player.getWorld(), player, blockInteraction.getBlockPosition(), blockInteraction)) {
                         bucketItem.useExtraContent(player.getWorld(), player, blockInteraction.getBlockPosition(), itemStack);
-                        player.awardStat(StatTypes.ITEM_USED.get(bucketItem));
                         return InteractionResult.SUCCESS;
                     }
                 }
