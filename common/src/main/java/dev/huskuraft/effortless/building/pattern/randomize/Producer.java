efa5fe5df3cad73013c448d416cda29a46ceefc0
huskcasaca <100605532+huskcasaca@users.noreply.github.com>
2024-05-14 16:42:37 +0800

    refactor: RandomProducer

--- common/src/main/java/dev/huskuraft/effortless/building/pattern/randomize/Producer.java
+++ common/src/main/java/dev/huskuraft/effortless/building/pattern/randomize/Producer.java
@@ -8,79 +8,55 @@ import java.util.stream.IntStream;
 public interface Producer<T> {
 
     static <T> Producer<T> createUnordered(Randomizer<T> randomizer, long seed, boolean consume) {
-        if (consume) {
-            return new ReduceUnordered<>(mapRandomizer(randomizer), seed);
-        }
-        return new Unordered<>(mapRandomizer(randomizer), seed);
+        return new Unordered<>(mapRandomizer(randomizer), seed, consume);
     }
 
     static <T> Producer<T> createSequence(Randomizer<T> randomizer, long seed, boolean consume) {
-        if (consume) {
-            return new ReduceSequence<>(mapRandomizer(randomizer), seed);
-        }
-        return new Sequence<>(mapRandomizer(randomizer));
+        return new Sequence<>(mapRandomizer(randomizer), consume);
     }
 
     static <T> Producer<T> createSingle(T item) {
         return new Single<>(item);
     }
 
-    @SuppressWarnings("unchecked")
-    private static <T> T[] mapRandomizer(Randomizer<T> randomizer) {
-        return (T[]) randomizer.getChances().stream().flatMap(holder -> IntStream.range(0, holder.chance()).mapToObj(i -> holder.content())).toArray(Object[]::new);
+    private static <T> List<T> mapRandomizer(Randomizer<T> randomizer) {
+        return randomizer.getChances().stream().flatMap(holder -> IntStream.range(0, holder.chance()).mapToObj(i -> holder.content())).toList();
     }
 
     T next();
 
-    // FIXME: 24/10/23
     class Unordered<T> extends Random implements Producer<T> {
 
-        private final T[] items;
+        private final List<T> items;
+        private final boolean consume;
 
-//        public Unordered(Randomizer<T> randomizer) {
-//            this(randomizer, newSeed() ^ System.nanoTime());
-//        }
-
-        public Unordered(T[] randomizer, long seed) {
+        public Unordered(List<T> items, long seed, boolean consume) {
             super(seed);
-            this.items = randomizer;
+            this.items = consume ? new ArrayList<>(items) : items;
+            this.consume = consume;
         }
 
         @Override
         public T next() {
-            if (items.length == 0) {
+            if (items.isEmpty()) {
                 return null;
             }
-            return items[nextInt(items.length)];
+            if (consume) {
+                return items.remove(nextInt(items.size()));
+            }
+            return items.get(nextInt(items.size()));
         }
     }
 
     class Sequence<T> implements Producer<T> {
 
-        private final T[] items;
+        private final List<T> items;
+        private final boolean consume;
         private int index = 0;
 
-        public Sequence(T[] items) {
-            this.items = items;
-        }
-
-        @Override
-        public T next() {
-            if (items.length == 0) {
-                return null;
-            }
-            return items[index++ % items.length];
-        }
-
-    }
-
-    class ReduceUnordered<T> extends Random implements Producer<T> {
-
-        private final List<T> items;
-
-        public ReduceUnordered(T[] items, long seed) {
-            super(seed);
-            this.items = new ArrayList<>(List.of(items));
+        public Sequence(List<T> items, boolean consume) {
+            this.items = consume ? new ArrayList<>(items) : items;
+            this.consume = consume;
         }
 
         @Override
@@ -88,27 +64,10 @@ public interface Producer<T> {
             if (items.isEmpty()) {
                 return null;
             }
-            int randomIndex = nextInt(items.size()); // Generate a random index
-            return items.remove(randomIndex);
-        }
-
-    }
-
-    class ReduceSequence<T> extends Random implements Producer<T> {
-
-        private final List<T> items;
-
-        public ReduceSequence(T[] items, long seed) {
-            super(seed);
-            this.items = new ArrayList<>(List.of(items));
-        }
-
-        @Override
-        public T next() {
-            if (items.isEmpty()) {
-                return null;
+            if (consume) {
+                return items.remove(0);
             }
-            return items.remove(0);
+            return items.get(index++ % items.size());
         }
 
     }
