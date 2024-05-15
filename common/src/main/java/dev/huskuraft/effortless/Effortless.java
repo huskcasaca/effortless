f79b540183f1485fc99e11217380d28059d3400b
huskcasaca <100605532+huskcasaca@users.noreply.github.com>
2024-05-15 23:57:58 +0800

    feat: Bump protocol version to 8

--- common/src/main/java/dev/huskuraft/effortless/Effortless.java
+++ common/src/main/java/dev/huskuraft/effortless/Effortless.java
@@ -12,7 +12,7 @@ import dev.huskuraft.effortless.api.text.Text;
 public class Effortless implements Entrance {
 
     public static final String MOD_ID = "effortless";
-    public static final int PROTOCOL_VERSION = 7;
+    public static final int PROTOCOL_VERSION = 8;
 
     private final CommonEventRegistry commonEventRegistry = (CommonEventRegistry) EventRegister.getCommon();
     private final EffortlessNetworkChannel networkChannel = new EffortlessNetworkChannel(this);
