11e12bef5a6fd5ac6918ad31abe9cb519536c643
huskcasaca <100605532+huskcasaca@users.noreply.github.com>
2024-06-25 11:34:58 +0800

    feat: Port to neoforge

--- api/src/main/java/dev/huskuraft/effortless/api/file/adapters/JsonFileAdapter.java
+++ api/src/main/java/dev/huskuraft/effortless/api/file/adapters/JsonFileAdapter.java
@@ -4,9 +4,6 @@ import java.io.File;
 import java.io.IOException;
 
 import com.electronwill.nightconfig.core.Config;
-import com.electronwill.nightconfig.core.io.WritingMode;
-import com.electronwill.nightconfig.json.FancyJsonWriter;
-import com.electronwill.nightconfig.json.JsonParser;
 
 import dev.huskuraft.effortless.api.file.FileAdapter;
 
@@ -14,12 +11,14 @@ public final class JsonFileAdapter extends FileAdapter<Config> {
 
     @Override
     public Config read(File file) throws IOException {
-        return new JsonParser().parse(file, (file1, configFormat) -> false);
+//        return new JsonParser().parse(file, (file1, configFormat) -> false);
+        throw new UnsupportedOperationException("JsonParser is not supported");
     }
 
     @Override
     public void write(File file, Config config) throws IOException {
-        new FancyJsonWriter().write(config, file, WritingMode.REPLACE);
+//        new FancyJsonWriter().write(config, file, WritingMode.REPLACE);
+        throw new UnsupportedOperationException("JsonWriter is not supported");
     }
 
 }
