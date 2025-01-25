b6632f71d966a5ac3f38f3f0cc36e659e3a69c3f
huskcasaca <100605532+huskcasaca@users.noreply.github.com>
2025-01-25 17:53:05 +0800

    chore: Add multiple versions

--- forge/mc-1-21/src/main/java/dev/huskuraft/effortless/vanilla/core/MinecraftPropertyValue.java
+++
@@ -1,9 +0,0 @@
-package dev.huskuraft.effortless.vanilla.core;
-
-import dev.huskuraft.effortless.api.core.PropertyValue;
-
-public record MinecraftPropertyValue(
-        Comparable<?> refs
-) implements PropertyValue {
-
-}
