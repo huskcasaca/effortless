bbfe9f67e731b0002ed113f2ff9c81ffa0681614
huskcasaca <100605532+huskcasaca@users.noreply.github.com>
2025-02-07 18:48:21 +0800

    refactor(plugin): Rename plugins

--- common/src/main/java/dev/huskuraft/effortless/building/interceptor/OpenPacInterceptor.java
+++ common/src/main/java/dev/huskuraft/effortless/building/interceptor/OpenPacInterceptor.java
@@ -5,18 +5,18 @@ import dev.huskuraft.effortless.api.core.Player;
 import dev.huskuraft.effortless.api.core.World;
 import dev.huskuraft.effortless.api.platform.ClientEntrance;
 import dev.huskuraft.effortless.api.platform.Entrance;
-import dev.huskuraft.effortless.api.plugin.openpac.ChunkClaimsManager;
+import dev.huskuraft.effortless.api.plugin.openpac.OpenPacChunkClaimsManager;
 import dev.huskuraft.effortless.api.plugin.openpac.OpenPacClientPlugin;
 import dev.huskuraft.effortless.api.plugin.openpac.OpenPacPlugin;
 
 public final class OpenPacInterceptor implements BuildInterceptor {
 
-    private final ChunkClaimsManager chunkClaimsManager;
+    private final OpenPacChunkClaimsManager openPacChunkClaimsManager;
 
     public OpenPacInterceptor(
-            ChunkClaimsManager chunkClaimsManager
+            OpenPacChunkClaimsManager openPacChunkClaimsManager
     ) {
-        this.chunkClaimsManager = chunkClaimsManager;
+        this.openPacChunkClaimsManager = openPacChunkClaimsManager;
     }
 
     public OpenPacInterceptor(
@@ -24,20 +24,20 @@ public final class OpenPacInterceptor implements BuildInterceptor {
     ) {
         if (entrance instanceof ClientEntrance) {
             var plugin = entrance.findPlugin(OpenPacClientPlugin.class);
-            this.chunkClaimsManager = plugin.map(OpenPacClientPlugin::getClaimManager).orElse(null);
+            this.openPacChunkClaimsManager = plugin.map(OpenPacClientPlugin::getClaimManager).orElse(null);
         } else {
             var plugin = entrance.findPlugin(OpenPacPlugin.class);
-            this.chunkClaimsManager = plugin.map(openPacPlugin -> openPacPlugin.getServerClaimManager(entrance.getServer())).orElse(null);
+            this.openPacChunkClaimsManager = plugin.map(openPacPlugin -> openPacPlugin.getServerClaimManager(entrance.getServer())).orElse(null);
         }
     }
 
-    public ChunkClaimsManager getChunkClaimsManager() {
-        return chunkClaimsManager;
+    public OpenPacChunkClaimsManager getChunkClaimsManager() {
+        return openPacChunkClaimsManager;
     }
 
     @Override
     public boolean isEnabled() {
-        return chunkClaimsManager != null;
+        return openPacChunkClaimsManager != null;
     }
 
     @Override
